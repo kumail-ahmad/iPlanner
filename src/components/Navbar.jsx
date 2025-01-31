a4b71adb46e772c0f361b47286d7a09e5c033bfa
Kumail Ahmad <kumailahmad217@gmail.com>
2025-01-31 19:49:37 +0530

    added spacing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     <div>
       <nav className=" bg-gray-700 rounded-md list-none">
         <div className="container md:w-3/2 flex gap-5 items-center text-white p-3 ">
-          <div className="logo">
+          <div className=" logo">
             <span className="font-bold text-2xl mx-9">iPlan</span>
           </div>
           <a className="hover:font-bold" href="/">
